8dc1ee43467c17e1135a7e8cc06703db1c4d60e2
Lisa J. Nystad <89862993+LisaJN@users.noreply.github.com>
2021-08-31 18:41:05 +0200

    Add files via upload

---
+++ massdensity.py
@@ -0,0 +1,21 @@
+#massetetthet definert som kg/m**3
+x = 0.858 #kg
+y = 4*10**(-5) #m**3
+
+massetetthet = x/y
+
+print('massetetthet av kuben er %.3f kg/m^3' % massetetthet)
+
+"""
+ekstern35:desktop lisa$ Python massdensity.py
+massetetthet av kuben er 21450.000 kg/m^3
+ekstern35:desktop lisa$
+"""
+#Massetetthet (i kg /m 3)
+#Polystyren(lav tetthet) = 20
+#Kork = 220
+#Rhenium = 21020
+#Platinium = 21450
+"""
+Ser ut fra tabellen at det likner mest på Platinium lik 21450 kg/m^3
+"""
---
+++ seconds2years.py
@@ -0,0 +1,11 @@
+billion = 10**9 #sekunder
+yr = 31556926 #sekunder
+
+liv = billion/yr
+print('baby kan leve i %.3f år' % liv)
+
+"""
+Lisas-MacBook-Air:desktop lisa$ Python seconds2years.py
+baby kan leve i 31.689 år
+Lisas-MacBook-Air:desktop lisa$
+"""
