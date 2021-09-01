be096b28248e66e46e210e65572083be561afe66
Lisa J. Nystad <89862993+LisaJN@users.noreply.github.com>
2021-09-01 11:39:24 +0200

    Create f2c_cml.py

---
+++ uke 4/f2c_cml.py
@@ -0,0 +1,16 @@
+import sys
+
+F = sys.argv[1]
+
+
+F = float(F)
+C = (F-32)*5/9
+print(f'{F:g} degrees fahrenheit in {C:g} celsius')
+
+"""
+import sys
+print(sys.argv)
+
+#arg1 = float(sys.argv[1])
+#print(arg1)
+"""
