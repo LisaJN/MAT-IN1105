938a1ab9810c791f176624655a4ee3b4d58f255a
Lisa J. Nystad <89862993+LisaJN@users.noreply.github.com>
2021-09-06 18:36:00 +0200

    Update sum_for.py

--- uke 3/sum_for.py
+++ uke 3/sum_for.py
@@ -3,15 +3,15 @@ s = 0
 M = 3
 k = 1
 
-for i in range(M):
+for k in range(1,M+1):
     s += 1/((2*k)**2)
-    k += 1
 print(s)
+
 """
-Terminal>sum_for.py
+Terminal> sum_for.py
 0.3402777777777778
 """
 """
-feil: definer k, sette inn () i formelen slik at den regner ut riktig,
-legge til en k for hver runde gjennom loopen.
+feil: definer k, sette inn () i formelen slik at den regner ut riktig, sette
+range fra 1 til M+1
 """
