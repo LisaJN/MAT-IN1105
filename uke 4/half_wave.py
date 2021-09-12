9668846d0aff81b80709ca9f9acff74db15a4f95
Lisa J. Nystad <89862993+LisaJN@users.noreply.github.com>
2021-09-12 12:31:24 +0200

    Update half_wave.py

--- uke 4/half_wave.py
+++ uke 4/half_wave.py
@@ -16,7 +16,13 @@ def test_f():
     computed,computed2 = f(np.pi/2),f(3*np.pi/2)
     tol = 1E-14
     success = abs(expected - computed) < tol
-    msg = f"computed={computed} != {expected}(expected)"
-    msg = f"computed={computed2} != {expected2}(expected)"
+    success2 = abs(expected2 - computed2) < tol
+    msg = f'computed={computed} != {expected}(expected)'
+    msg2 = f'computed={computed2} != {expected2}(expected)'
     assert success, msg
+    assert success2, msg2
 test_f()
+
+"""
+Terminal>half_wave.py
+"""
