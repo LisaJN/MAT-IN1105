46bda8019405881afd14485eaa7c685eaf710497
Lisa J. Nystad <89862993+LisaJN@users.noreply.github.com>
2021-09-21 17:12:17 +0200

    Update quadratic_roots_error.py

--- uke 5/quadratic_roots_error.py
+++ uke 5/quadratic_roots_error.py
@@ -8,23 +8,23 @@ try:
     c = float(sys.argv[3])
 
 except IndexError:
-    print("too few arguemnts")
-    a = float(input("please enter value of a: "))
-    b = float(input("please enter value of b: "))
-    c = float(input("please enter value of c: "))
+    print('too few arguemnts')
+    a = float(input('please enter value of a: '))
+    b = float(input('please enter value of b: '))
+    c = float(input('please enter value of c: '))
 
 x_1 = (((-b)+(np.sqrt((b**2)-(4*a*c))))/(2*a))
 x_2 = (((-b)-(np.sqrt((b**2)-(4*a*c))))/(2*a))
-print(x_1,x_2)
+print(f'{x_1:.2f}',f'{x_2:.2f}')
 
 """
 Terminal>quadratic_roots_error.py -1 0 1
--1.0 1.0
+-1.00 1.00
 
 Terminal>quadratic_roots_error.py
 too few arguemnts
 please enter value of a: -6
 please enter value of b: -1
 please enter value of c: 2
--0.666666666667 0.5
+-0.67 0.50
 """
