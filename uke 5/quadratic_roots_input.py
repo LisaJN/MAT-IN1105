c1a1e4f2a30b6269bb5c19da2659863c293a368b
Lisa J. Nystad <89862993+LisaJN@users.noreply.github.com>
2021-09-21 17:10:41 +0200

    Update quadratic_roots_input.py

--- uke 5/quadratic_roots_input.py
+++ uke 5/quadratic_roots_input.py
@@ -1,20 +1,19 @@
 #5.1
 import numpy as np
 
-a = float(input("please enter value of a: "))
-b = float(input("please enter value of b: "))
-c = float(input("please enter value of c: "))
+a = float(input('please enter value of a: '))
+b = float(input('please enter value of b: '))
+c = float(input('please enter value of c: '))
 
 x_1 = (((-b)+(np.sqrt((b**2)-(4*a*c))))/(2*a))
 x_2 = (((-b)-(np.sqrt((b**2)-(4*a*c))))/(2*a))
 
-print(x_1,x_2)
+print(f'{x_1:.2f}',f'{x_2:.2f}')
 
 """
 Terminal>quadratic_roots_input.py
 please enter value of a: -6
 please enter value of b: -1
 please enter value of c: 2
-<class 'float'>
--0.666666666667 0.5
+-0.67 0.50
 """
