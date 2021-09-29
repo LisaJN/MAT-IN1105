6cd221db121e9f2087ac2837fd157225689868e7
Lisa J. Nystad <89862993+LisaJN@users.noreply.github.com>
2021-09-29 22:04:02 +0200

    Update oscillating_spring.py

--- uke 6/oscillating_spring.py
+++ uke 6/oscillating_spring.py
@@ -16,17 +16,15 @@ def y(t):
 t_array = np.zeros(n) #tom array
 y_array = np.zeros(n) #tom array
 
-for i in range(n): #lager en for-løkke som fyller arrayene med y og t verdier
+for i in range(n):    #lager en for-løkke som fyller arrayene med y og t verdier
     t_array[i] = i*dt
     y_array[i] = y(t_array[i])
 
-
 t_array_2 = np.linspace(0,24,101) #lager en ny t_array med linspace
-y_array_2 = y(t_array_2) #lager en ny y_array og kaller på y(t) med nye t_array
-
+y_array_2 = y(t_array_2)          #lager en ny y_array og kaller på y(t) med nye t_array
 
 plt.plot(t_array,y_array, 'mo')
-plt.plot(t_array_2,y_array_2)
+plt.plot(t_array_2,y_array_2, 'c')
 plt.title('Oscillating Spring')
 plt.xlabel('tid [s]')
 plt.ylabel('posisjon [m]')
@@ -36,4 +34,4 @@ plt.show()
 """
 Terminal>oscillating_spring.py
 """
-#se vedlegg: oscillating_spring.png 
+#se vedlegg: oscillating_spring.png
