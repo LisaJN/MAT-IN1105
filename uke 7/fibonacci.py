2717e95bf6baec4136dbaff91093e7d2aa4653a3
Lisa J. Nystad <89862993+LisaJN@users.noreply.github.com>
2021-10-07 13:01:39 +0200

    Update fibonacci.py

--- uke 7/fibonacci.py
+++ uke 7/fibonacci.py
@@ -1,26 +1,26 @@
 #A.2
 def x(n):
-    if n==0 or n==1:
+    if n==0 or n==1:       #Setter de to første elementene lik 1
         return 1
-    return x(n-1) + x(n-2)
-for n in range(0,15):
-    print('x_%g = %g' %(n, x(n)))
+    return x(n-1) + x(n-2) #Setter opp diff likningen
+for n in range(0,15):      #Velger de 15 første elementene
+    print('x%g = %g' %(n,x(n)))
 
 """
 Terminal>fibonacci.py
-x_0 = 1
-x_1 = 1
-x_2 = 2
-x_3 = 3
-x_4 = 5
-x_5 = 8
-x_6 = 13
-x_7 = 21
-x_8 = 34
-x_9 = 55
-x_10 = 89
-x_11 = 144
-x_12 = 233
-x_13 = 377
-x_14 = 610
+x0 = 1
+x1 = 1
+x2 = 2
+x3 = 3
+x4 = 5
+x5 = 8
+x6 = 13
+x7 = 21
+x8 = 34
+x9 = 55
+x10 = 89
+x11 = 144
+x12 = 233
+x13 = 377
+x14 = 610
 """
